8acede4d601105ca5f582fe1a4a2c50c99e28c6c
Disoul507 <62339531+Disoul507@users.noreply.github.com>
2022-03-15 15:17:15 +0300

    Update 1.rb

--- 1.rb
+++ 1.rb
@@ -1,2 +1,25 @@
 puts "hello world"
-puts "test"
+
+ puts "Hello, #{ARGV[0]}."
+
+puts "what is your favorite programming language?"
+name= gets.chomp
+#if name == "ruby"
+    #puts"You're a flatterer"
+#else 
+    #puts"It's time to choose the language you like"
+#end
+
+case name
+   when "ruby", "Ruby"
+    puts"Don't Flatter"
+
+   when"PHP", "php"
+    puts"Do you luke scripts?"
+    
+   when "Python", "python"
+    puts"I love Simplicity"
+    
+   else 
+    puts"This is bad"
+end
