921b941598d8a8e8044edbbeee8f506e47eb5d5b
Disoul507 <62339531+Disoul507@users.noreply.github.com>
2022-03-15 17:58:41 +0300

    Update 1.2.rb

--- 1.2.rb
+++ 1.2.rb
@@ -7,13 +7,47 @@ puts "Число равно #{n}"
     
 #end
 #puts s
-def sum_digits
+def sum_digits(n)
     s=0
     while n!=0
          s+=n%10
          n=n/10
     end
-    #puts "сумма цифр равна #{s}"
-    return s
+    puts ("sum #{s}")
 end
 puts sum_digits(n)
+
+def min_digits(n)
+    min=9
+    while n!=0
+        if n%10<min
+            min=n%10
+        end    
+         n=n/10
+    end
+    puts("min #{min}")
+end
+puts min_digits(n)
+
+
+def max_digits(n)
+    max=0
+    while n!=0
+        if n%10>max
+         max=n%10
+        end
+         n=n/10
+    end
+  puts  ("max #{max}")
+end
+puts max_digits(n)
+
+def multi_digits(n)
+    s=1
+    while n!=0
+         s*=n%10
+         n=n/10
+    end
+   puts ("multi #{s}")
+end
+puts multi_digits(n)
