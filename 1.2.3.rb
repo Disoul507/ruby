a44f76b6dc8d36efe861ba02cc41de3345f4ac6e
Disoul507 <62339531+Disoul507@users.noreply.github.com>
2022-03-16 15:42:33 +0300

    Update 1.2.3.rb
    Максимально простой делитель числа

--- 1.2.3.rb
+++ 1.2.3.rb
@@ -1,5 +1,4 @@
-n= ARGV[0].to_i
-puts "Число равно #{n}" 
+require "prime"
 def multi_digits_five(n)
     s=1
     while n!=0
@@ -8,6 +7,52 @@ def multi_digits_five(n)
         end
          n=n/10
     end
-   puts ("multi #{s}")
+   return s
 end
-puts multi_digits_five(n)
+
+def delitels(n)
+    det=[n]
+    for i in 1..(n/2)
+        if n % i==0
+            det<<i
+        end
+    end
+   return det
+end
+
+def prost_delit(d)
+    det1=[]
+    for i in 0..(d.size-1)
+        if d[i].prime? 
+            det1<<d[i]
+        end
+    end
+    return det1
+end    
+
+def max_pr(p)
+    max=0
+    for i in 0..(p.size-1)
+        if p[i]>max
+            max=p[i]
+        end
+    end
+    return max
+end
+
+def execute_max(n)
+    d = delitels(n)
+    p=prost_delit(d)
+    max_pr(p)
+end
+
+n= ARGV[0].to_i
+puts "Число равно #{n}" 
+
+ #d=delitels(n)
+#puts "делители#{d}"
+#p=prost_delit(d)
+#puts "простые делители #{p}"
+#puts max(p)
+
+#puts multi_digits_five(n)
