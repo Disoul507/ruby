9867c213f70020267b598c47670768e939c00588
Disoul507 <62339531+Disoul507@users.noreply.github.com>
2022-03-19 14:52:55 +0300

    Update 1.2.3.rb
    Добавлен 7 метод

--- 1.2.3.rb
+++ 1.2.3.rb
@@ -30,6 +30,7 @@ def prost_delit(d)
     return det1
 end    
 
+
 def max_pr(p)
     max=0
     for i in 0..(p.size-1)
@@ -46,13 +47,69 @@ def execute_max(n)
     max_pr(p)
 end
 
+def nechet(d)
+    det=[]
+    for i in 0..(d.size-1)
+        if d[i] % 2!=0
+            det<<d[i]
+        end
+    end
+   return det
+end
+
+def neprost_delit(nc)
+    det1=[]
+    for i in 0..(nc.size-1)
+        if !nc[i].prime? && nc[i]!=1
+            det1<<nc[i]
+        end
+    end
+    return det1
+end 
+
+def multi_digits(n)
+    s=1
+    while n!=0
+         s*=n%10
+         n=n/10
+    end
+   return s
+end
+
+def nod_evklid(m,max)
+    if m>max
+        a=m
+        b=max
+    else 
+        a=max
+        b=m
+    end
+    if b==0
+        return a 
+    end
+    if b==1
+        return 1
+    end
+    nod_evklid(b,a%b)
+  end
+
+def execute_nod(n)
+    d = delitels(n)
+    puts "делители #{d}"
+    nc=nechet(d)
+    puts " нечетные делители#{nc}"
+    ne=neprost_delit(nc)
+    puts " непростые делители #{ne}"
+    max=max_pr(ne)
+    puts "максимальный непростой делитель #{max}"
+    m=multi_digits(n)
+    puts "произведение цифр #{m}"
+    nod_evklid(m,max)
+ end
+    
 n= ARGV[0].to_i
 puts "Число равно #{n}" 
 
- #d=delitels(n)
-#puts "делители#{d}"
-#p=prost_delit(d)
-#puts "простые делители #{p}"
-#puts max(p)
-
-#puts multi_digits_five(n)
+puts "NOD: #{execute_nod(n)}"
+#puts "Максималный простой делитель: #{execute_max(n)}"
+#puts "Произведение цифр не равных 5: #{multi_digits_five(n)}"
