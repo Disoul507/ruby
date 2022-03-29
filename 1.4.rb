7cbddbb270a52709cd5d97569b386afa5449f093
Disoul507 <62339531+Disoul507@users.noreply.github.com>
2022-03-29 19:29:07 +0300

    Update 1.4.rb
    3 метод

--- 1.4.rb
+++ 1.4.rb
@@ -4,19 +4,35 @@ def read()
     return arr.map{|el|el.to_i}
 end
 
+def inter()
+    puts"Введите начало интервала"
+    a = gets.chomp()
+    puts"Введите конец интервала"
+    b = gets.chomp()
+    arr=(a..b).to_a
+    return arr.map{|el|el.to_i}
+end
+
 def min(arr)
     n= arr.size
    ind = arr.rindex(arr.min) 
-   arr2=arr.slice!(ind..n)
+   arr.slice!(ind..n)
     return arr.to_s
 end
 
 def max(arr)
     ind = arr.index(arr.max)
-    arr2=arr.slice!(0..ind)
-    #arr2=arr.slice!(0..(arr.max))
+    arr.slice!(0..ind)
     return arr.to_s
 end
+
+def sum_inter(arr,arr1)
+    arr2= arr & arr1
+    a=arr2.reduce{|result,el| result+=el}
+    return a
+end
 arr=read()
+arr1=inter()
 #puts "Массив чисел перед последним минимальным: #{min(arr)}"
-puts "Массив чисел после первого максимального: #{max(arr)}"
+#puts "Массив чисел после первого максимального: #{max(arr)}"
+puts "Сумма элементов попадающих в интервал: #{sum_inter(arr,arr1)}"
