d6f0264b485e875fe6667b5ecdd3d2bb187410bd
Disoul507 <62339531+Disoul507@users.noreply.github.com>
2022-03-29 17:53:19 +0300

    Update 1.4.rb
    добавлен  второй метод

--- 1.4.rb
+++ 1.4.rb
@@ -6,9 +6,17 @@ end
 
 def min(arr)
     n= arr.size
-   a = arr.rindex(arr.min) 
-   arr2=arr.slice!(a..n)
+   ind = arr.rindex(arr.min) 
+   arr2=arr.slice!(ind..n)
+    return arr.to_s
+end
+
+def max(arr)
+    ind = arr.index(arr.max)
+    arr2=arr.slice!(0..ind)
+    #arr2=arr.slice!(0..(arr.max))
     return arr.to_s
 end
 arr=read()
-puts "Массив чисел перед последним минимальным: #{min(arr)}"
+#puts "Массив чисел перед последним минимальным: #{min(arr)}"
+puts "Массив чисел после первого максимального: #{max(arr)}"
